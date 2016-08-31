b873c05ab817ec3c7d3f150f91b9b2634afea193
Ace P Rbk <rbk.unlimited@gmail.com>
2016-08-31 15:50:30 +0000

    Starter Boot Project Initialization

---
+++ src/test/java/com/swiftpot/projectuknown/ProjectUknownApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swiftpot.projectuknown;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectUknownApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
